7adc91839939a101d70e2121d915bc48fa0fc52b
kylenngu135 <kylenthefrog@gmail.com>
2026-01-19 11:14:24 -0800

    added game engine, main.js, and index.html files, asset folder with test asset inside

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const gameEngine = new GameEngine();
+
+const ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("../assets/16x16/16x16 Idle-Sheet.png");
+//ASSET_MANAGER.queueDownload("");
+//ASSET_MANAGER.queueDownload("");
+//ASSET_MANAGER.queueDownload("");
+
+ASSET_MANAGER.downloadAll(() => {
+	const canvas = document.getElementById("gameWorld");
+	const ctx = canvas.getContext("2d");
+
+	gameEngine.init(ctx);
+
+	gameEngine.start();
+});
