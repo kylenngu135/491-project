92342eb66332e9af1a291f27c1a7c8895f53cf4b
kylenngu135 <kylenthefrog@gmail.com>
2026-01-26 15:24:25 -0800

    Added player tracking for all current enemies

--- src/entities/minotaur.js
+++ src/entities/minotaur.js
@@ -1,5 +1,5 @@
 const MINOTAUR_IDLE_PATH = "./assets/Tiny Swords (Enemy Pack)/Enemy Pack/Minotaur/Minotaur_Idle.png";
-const MINOTAUR_WALK_PATH = "./assets/Tiny Swords (Enemy Pack)/Enemy Pack/Minotaur/Minotaur_Walk.png";
+const MINOTAUR_RUN_PATH = "./assets/Tiny Swords (Enemy Pack)/Enemy Pack/Minotaur/Minotaur_Walk.png";
 const MINOTAUR_ATTACK_PATH = "./assets/Tiny Swords (Enemy Pack)/Enemy Pack/Minotaur/Minotaur_Attack.png";
 const MINOTAUR_GUARD_PATH = "./assets/Tiny Swords (Enemy Pack)/Enemy Pack/Minotaur/Minotaur_Guard.png";
 
@@ -7,14 +7,20 @@ const MINOTAUR_GUARD_PATH = "./assets/Tiny Swords (Enemy Pack)/Enemy Pack/Minota
 
 const MINOTAUR_STATE = {
     IDLE: 0,
-    WALK: 1,
+    RUN: 1,
     ATTACK: 2,
-    GAURD: 3,
+    GAURD: 3
 }
 
 class Minotaur extends Entity {
-    constructor(game, destX, destY) {
+    constructor(game, destX, destY, target) {
         super(game, MINOTAUR_STATE, 0, 0, 320, 320, destX, destY, 192, 192, Minotaur.#getSpriteSheets());
+
+        // i dont have anything for this rn but it will be how far the lizard can see 
+        this.visualRadius = 200;
+        this.target = target;
+        //this is in the video
+        this.maxSpeed = 150;
     }
 
     static #getSpriteSheets() {
@@ -24,7 +30,7 @@ class Minotaur extends Entity {
                 frame_count: 16
             },
             {
-                sheet: ASSET_MANAGER.getAsset(MINOTAUR_WALK_PATH),
+                sheet: ASSET_MANAGER.getAsset(MINOTAUR_RUN_PATH),
                 frame_count: 8
             },
             {
@@ -36,5 +42,42 @@ class Minotaur extends Entity {
                 frame_count: 11
             }
         ];
+    }
+
+    update(){
+        // Calculate the distance between the lizard and the warrior using the helper method
+        var dist = this.distance(this, this.target);
+        
+        // Calculate velocity to move toward the warriro this is what he uses
+        this.velocity = { 
+            x: (this.target.destX - this.destX) / dist * this.maxSpeed,
+            y: (this.target.destY - this.destY) / dist * this.maxSpeed 
+        };
+        
+        // Update the lizard's X position this is simmilar to what he used in his video 
+        this.destX += this.velocity.x * this.game.clockTick;
+        
+        // Update the lizard's Y position This is what he had in his video
+        this.destY += this.velocity.y * this.game.clockTick;
+        
+        // Set animation state to running this is hard coded we might want to change it later 
+        this.state = MINOTAUR_STATE.RUN;
+        
+        // Stop moving if really close to the warrior we need to change this later maybe make a monster file that
+        // extends entities and then extend that to monsters 
+        if(dist < 5){
+            this.velocity = { x: 0, y: 0 };
+            this.state = MINOTAUR_STATE.IDLE;
+        }
+    
+        this.updateDirection();
+    }
+
+    // Helper method to calculate distance between two entities using Pythagorean theorem this is what i am assuming it is doiing in the vid
+    // i just looked up how and this is it. 
+    distance(entity1, entity2) {
+        var dx = entity1.destX - entity2.destX;
+        var dy = entity1.destY - entity2.destY;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 }
