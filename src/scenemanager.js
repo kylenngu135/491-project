f3e70979f7f4725794f1169e76b37f0ccf18a744
justin <justiny143s@gmail.com>
2026-01-26 17:57:56 -0800

    made a shaman and tried to make a lancer

--- src/scenemanager.js
+++ src/scenemanager.js
@@ -3,11 +3,13 @@ class SceneManager {
         this.game = game;
         this.background = new Background();
         this.warrior = new Warrior(this.game, 0, 0);
+        //this.warrior = new Lancer(this.game, 0, 0);
         this.minotaur = new Minotaur(this.game, 100, 25, this.warrior);
         this.thief = new Thief(this.game, 250, 50, this.warrior);
         this.lizard = new Lizard(this.game, 500, 20, this.warrior);
         this.paddle_fish = new PaddleFish(this.game, 300, 70, this.warrior);
         this.troll = new Troll(this.game, 500, 50, this.warrior);
+        this.shaman = new Shaman(this.game, 400, 30, this.warrior);
         
     }
 
@@ -19,6 +21,7 @@ class SceneManager {
         this.game.addEntity(this.paddle_fish);
         this.game.addEntity(this.lizard);
         this.game.addEntity(this.warrior);
+        this.game.addEntity(this.shaman);
     }
 
     draw(ctx) {
@@ -27,7 +30,7 @@ class SceneManager {
         this.paddle_fish.draw(ctx);
         this.lizard.draw(ctx);
         this.troll.draw(ctx);
-
+        this.shaman.draw(ctx);
         this.minotaur.draw(ctx);
         this.warrior.draw(ctx);
     }
@@ -40,5 +43,6 @@ class SceneManager {
         this.troll.update();
         this.minotaur.update();
         this.warrior.update();
+        this.shaman.update();
     }
 }
