a3ea5892598a8d5e9e360060c93aaa1cfb9c78c2
kylenngu135 <kylenthefrog@gmail.com>
2026-01-26 18:13:42 -0800

    fixed lancer bugs

--- src/entities/lancer.js
+++ src/entities/lancer.js
@@ -2,7 +2,7 @@ const LANCER_IDLE_PATH = "./assets/Tiny Swords (Free Pack)/Units/Blue Units/Lanc
 const LANCER_RUN_PATH = "./assets/Tiny Swords (Free Pack)/Units/Blue Units/Lancer/Lancer_Run.png";
 const LANCER_ATTACK1_PATH = "./assets/Tiny Swords (Free Pack)/Units/Blue Units/Lancer/Lancer_Right_Attack.png";
 const LANCER_GUARD_PATH = "./assets/Tiny Swords (Free Pack)/Units/Blue Units/Lancer/Lancer_Right_Defence.png";
-const STATE = {
+const LANCER_STATE = {
     IDLE: 0,
     RUN: 1,
     ATTACK1: 2,
@@ -13,7 +13,7 @@ const STATE = {
 
 class Lancer extends Hero {
     constructor(game, destX, destY) {
-        super(game, STATE, 0, 0, 192, 192, destX, destY, 192, 192, Lancer.#getSpriteSheets(), false, null, 6 * FRAME_DURATION);
+        super(game, LANCER_STATE, 0, 0, 320, 320, destX, destY, 192, 192, Lancer.#getSpriteSheets(), false, null, 6 * FRAME_DURATION);
     }
 
     static #getSpriteSheets() {
